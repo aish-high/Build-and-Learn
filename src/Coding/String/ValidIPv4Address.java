e19648a31a5e8d500812988f0fadfd0af02ce6c8
aish-high <50437672+aish-high@users.noreply.github.com>
2025-10-10 19:43:55 +0530

    - correct ipv4 validation pattern

--- src/Coding/String/ValidIPv4Address.java
+++ src/Coding/String/ValidIPv4Address.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class ValidIPv4Address {
     public static void main(String[] args) {
         String ip = "172.0.0.000";
-        Pattern pattern = Pattern.compile("[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}");
+        Pattern pattern = Pattern.compile("^(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}$");
         System.out.println("Is input a valid ipv4 address: " + pattern.matcher(ip).matches());
     }
 }
